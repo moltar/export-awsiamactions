8e28c260b9abb95583c9b4873aa5a23e6f85ea17
Roman Filippov <rf@romanfilippov.com>
2024-06-16 18:14:46 +0100

    feat: demo run

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-export class Hello {
-  public sayHello() {
-    return 'hello, world!';
-  }
+import playwright from 'playwright';
+
+async function main () {
+  const browser = await playwright.chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto('https://amazon.com');
+  await page.waitForTimeout(1000);
+  await browser.close();
 }
+
+main().catch((err)=>console.error(err));
