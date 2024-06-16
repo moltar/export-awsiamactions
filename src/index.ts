883506aa12768a25fbd207de1cbf4e293053f0c8
Roman Filippov <rf@romanfilippov.com>
2024-06-16 18:24:51 +0100

    feat: impl save

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 import playwright from 'playwright';
 
 async function main () {
-  const browser = await playwright.chromium.launch();
+  const browser = await playwright.chromium.launch({
+    headless: false,
+  });
   const context = await browser.newContext();
   const page = await context.newPage();
-  await page.goto('https://amazon.com');
-  await page.waitForTimeout(1000);
+
+  // Load page
+  await page.goto('https://www.awsiamactions.io/');
+  await page.waitForLoadState('networkidle');
+
+  // Save
+  const downloadPromise = page.waitForEvent('download');
+  await page.getByText('Save As...').click();
+  const download = await downloadPromise;
+  await download.saveAs('awsiamactions.json');
+
   await browser.close();
 }
 
