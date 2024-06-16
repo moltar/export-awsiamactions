03cc1a3a7ad1c73fdb1dd3a75c5934d67c329bc7
Roman Filippov <rf@romanfilippov.com>
2024-06-16 18:08:18 +0100

    chore: use pnpm v9.3.0

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,8 +1,11 @@
 import { javascript, typescript } from 'projen';
+
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
   name: 'export-awsiamactions',
   packageManager: javascript.NodePackageManager.PNPM,
   projenrcTs: true,
+  pnpmVersion: '9.3.0',
 });
+
 project.synth();
